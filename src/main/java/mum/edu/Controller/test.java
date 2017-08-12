b6cf42d720d198075cdc89255d103a5bcdb9c609
Niroj Koirala <nirojk9@gmail.com>
2017-08-11 23:37:02 -0500

    commiting views and css

--- src/main/java/mum/edu/Controller/test.java
+++ src/main/java/mum/edu/Controller/test.java
@@ -1,16 +1,17 @@
 package mum.edu.Controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by Hatake on 8/11/2017.
  */
-@RestController
+@Controller
 public class test {
 
     @RequestMapping("/test")
     public String helloWorld(){
-        return "Hello from Spring Boot";
+        return "/addProduct";
     }
 }
